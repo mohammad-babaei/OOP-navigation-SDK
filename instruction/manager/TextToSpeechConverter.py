71e79e6c763b0d28081a6d45204f13b5277e5d72
unknown <payamyarandi98@gmail.com>
2021-02-06 02:55:20 +0330

    main.py added. bug in InstructionSetManager fixed

--- instruction/manager/TextToSpeechConverter.py
+++ instruction/manager/TextToSpeechConverter.py
@@ -1,28 +1,28 @@
-from interface import Interface, implements
+# from interface import Interface, implements
 from instruction.entity.Audio import Audio
 from instruction.entity.Instruction import Instruction
 
 
-class TextToSpeechConverter(Interface):
+class TextToSpeechConverter:
     def convertTextToVoice(self, instruction: Instruction):
         pass
 
 
-class PersianConverter(implements(TextToSpeechConverter)):
+class PersianConverter(TextToSpeechConverter):
     #the method is mocked
     def convertTextToVoice(self, instruction: Instruction):
         audio = Audio('Be Raast Bepichid')
         return audio
 
 
-class EnglishConverter(implements(TextToSpeechConverter)):
+class EnglishConverter(TextToSpeechConverter):
     #the method is mocked
     def convertTextToVoice(self, instruction: Instruction):
         audio = Audio('Turn Right')
         return audio
 
 
-class ArabicConverter(implements(TextToSpeechConverter)):
+class ArabicConverter(TextToSpeechConverter):
     #the method is mocked
     def convertTextToVoice(self, instruction: Instruction):
         audio = Audio('Aneataf Yamina')
