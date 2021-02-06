e88400c4ec4db3cbc0afb5eafb453722288caa0d
Mobin Dariush <mobindariush@outlook.com>
2021-02-06 17:06:19 +0330

    Fix type issues in LocationLog

--- liveTraffic/model/LocationLog.py
+++ liveTraffic/model/LocationLog.py
@@ -1,10 +1,23 @@
 from mapData.entity.LatLng import LatLng
+from datetime import datetime
 
 
 class LocationLog:
 
-    def __init__(self, userId: str, location: LatLng, datetime, speed: float):
+    def __init__(self, userId: str, location: LatLng, datetime: datetime, speed: float):
         self.userId = userId
         self.location = location
         self.datetime = datetime
         self.speed = speed
+
+    def getUserId(self) -> str:
+        return self.userId
+
+    def getLocation(self) -> LatLng:
+        return self.location
+
+    def getDateTime(self) -> datetime:
+        return self.datetime
+
+    def getSpeed(self) -> float:
+        return self.speed
